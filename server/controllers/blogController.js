f96ec3905694808e3d57ef6751453c48aeb978a9
Anuragx456 <anuragcolab0@gmail.com>
2026-02-03 15:12:16 +0530

    Basic Backend API

---
+++ server/controllers/blogController.js
@@ -0,0 +1,89 @@
+import fs from 'fs';
+import imagekit from '../configs/imageKit.js';
+import Blog from '../models/Blog.js';
+
+export const addBlog = async (req, res) => {
+    try {
+        const {title, subTitle, description, category, isPublished} = JSON.parse(req.body.blog);
+        const imageFile = req.file;
+
+        // Check if all fields are present
+        if(!title || !description || !category || !imageFile){
+            return res.json({success: false, message: "Missing required fields"})
+        }
+
+        const fileBuffer = fs.readFileSync(imageFile.path)
+
+        // Upload Image to ImageKit
+        const  response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile,
+            folder: "/blogs"
+        })
+
+        // Optimization through imagekit URL transformation 
+        const optimizedImageUrl = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {quality: 'auto'}, // Automatically compress image quality
+                {format: 'webp'}, // Change image format to .webp 
+                {width: '1280'} // Automatically resize image width to 1280px
+            ]
+        });
+
+        const image = optimizedImageUrl;
+
+        await Blog.create({title, subTitle, description, category, image, isPublished})
+
+        res.json({success: true, message: "Blog added successfully"})
+
+    } catch (error) {
+            res.json({success: false, message: error.message})
+    }
+}
+
+export const getAllBlogs = async (req, res) => {
+    try {
+        const blogs = await Blog.find({isPublished: true})
+        res.json({success: true, blogs})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const getBlogById = async(req, res) => {
+    try {
+        const { blogId } = req.params;
+        const blog = await Blog.findById(blogId);
+
+        if(!blog){
+            return res.json({success: false, message: "Blog not found"});
+        }
+        res.json({success: true, blog})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const deleteBlogById = async(req, res) => {
+    try {
+        const { id } = req.body;
+        await Blog.findByIdAndDelete(id);
+        res.json({success: true, message: "Blog deleted successfully"})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+
+export const togglePublish = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const blog = await Blog.findById(id);
+        blog.isPublished = !blog.isPublished;
+        await blog.save();
+        res.json({success: true, message: "Blog status updated"})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
