a39548df9d77eb7402ba555756787c6cb00fd2d6
Anuragx456 <anuragcolab0@gmail.com>
2026-02-06 16:05:03 +0530

    Backend & Frontend Connected

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react'
+import toast from 'react-hot-toast';
 import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill';
+import { useAppContext } from '../../context/AppContext';
 
 const AddBlog = () => {
+
+  const {axios} = useAppContext();
+  const [isAdding, setIsAdding] = useState(false);
 
   const editorRef = useRef(null);
   const quillRef = useRef(null);
@@ -18,7 +23,37 @@ const AddBlog = () => {
   }
 
   const onSubmitHandler = async (e) => {
-    e.preventDefault();
+    try {
+      e.preventDefault();
+      setIsAdding(true)
+
+      const blog = {
+        title, subTitle, 
+        description: quillRef.current.root.innerHTML, 
+        category, isPublished 
+      }
+
+      const formData = new FormData();
+      formData.append('blog', JSON.stringify(blog))
+      formData.append('image', image)
+
+      const {data} = await axios.post('/api/blog/add', formData);
+
+      if(data.success){
+        toast.success(data.message);
+        setImage(false)
+        setTitle('')
+        quillRef.current.root.innerHTML = ''
+        setCategory('Startup')
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+    finally{
+      setIsAdding(false )
+    }
   }
 
   useEffect(() => {
@@ -73,7 +108,10 @@ const AddBlog = () => {
             onChange={e => setIsPublished(e.target.checked)}/>
           </div>
 
-          <button type='submit' className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'>Add Blog</button>
+          <button disabled={isAdding} type='submit' className='mt-8 w-40 h-10 bg-primary text-white
+           rounded cursor-pointer text-sm'>
+            {isAdding ? 'Adding...' : 'Add Blog'}
+          </button>
 
         </div>
     </form>
